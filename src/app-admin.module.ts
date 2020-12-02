e78e2ed0698489729daafd732d8c0d5e879dc69f
Paolo Tagliani <pablosproject@gmail.com>
2020-12-02 10:54:26 +0100

    Add admin panel authentication

--- src/app-admin.module.ts
+++ src/app-admin.module.ts
@@ -19,12 +19,29 @@ AdminBro.registerAdapter({ Database, Resource });
   imports: [
     SharedModule,
     AdminModule.createAdminAsync({
-      useFactory: () => ({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
         adminBroOptions: {
           rootPath: '/',
           resources: [Product, Category, Order, OrderProduct, OrderUser],
         },
+        auth: {
+          authenticate: async (email, password) => {
+            const adminEmail = configService.get<string>('ADMIN_EMAIL');
+            const adminPass = configService.get<string>('ADMIN_PASS');
+            if (email !== adminEmail || password !== adminPass) {
+              return Promise.resolve(null);
+            }
+
+            return Promise.resolve({
+              email: configService.get<string>('ADMIN_EMAIL'),
+            });
+          },
+          cookieName: 'admin_name',
+          cookiePassword: 'admin_password',
+        },
       }),
+      inject: [ConfigService],
     }),
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
